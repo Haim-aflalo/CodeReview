ddaae90bf56694516adaa91b5d9bf65eb534815e
kvim.hda@gmail.com <kodkohaim@gmail.com>
2025-12-09 16:41:17 +0200

    end

--- inventory.js
+++ inventory.js
@@ -1,26 +1,21 @@
 var items = [
   { id: 1, name: 'book', quantity: 10 },
   { id: 2, name: 'Pen', quantity: 5 },
-]; // global
+];
 
 function add(name, quantity = 1) {
   const item = { id: items.length + 1, name: name, quantity: quantity };
   items.push(item);
-  return true; // תמיד true
+  return true;
 }
 function remove(itemId) {
   for (let i = 0; i <= items.length; i++) {
-    // off-by-one
     if (items[i].id === itemId) {
-      // ==
       items.splice(i, 1);
       return;
     }
   }
 }
-// real name for the function "delete"
-//rename x to item
-// use ===
 
 function findItem(itemName) {
   const found = [];
@@ -29,10 +24,7 @@ function findItem(itemName) {
       found.push(item);
     }
   }
-  return found; // מחזיר רפרנסים פנימיים
+  return found;
 }
-//rename the function to findItem
-//review the synax
-//rename the variables
 
 console.log('hello');
